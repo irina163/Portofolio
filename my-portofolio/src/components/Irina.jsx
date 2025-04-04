a8a0ac2d4f4355cd8e69fcb78ac5d1f85878ab96
irina <ferentirina@gmail.com>
2025-04-04 11:06:06 +0100

    Fixed model shading, rig and animation.

--- my-portofolio/src/components/Irina.jsx
+++ my-portofolio/src/components/Irina.jsx
@@ -1,30 +1,74 @@
-import React, { useRef } from 'react'
-import { useGLTF } from '@react-three/drei'
+import React, { useRef, useEffect } from 'react'
+import { useGLTF, useAnimations } from '@react-three/drei'
+import { useCharacterAnimations } from '../contexts/AnimationContext'
 
 export default function Irina(props) {
-  const { nodes, materials } = useGLTF('./models/Irina_face14.glb')
+  const group = useRef()
+  const { nodes, materials, animations } = useGLTF('./models/Irina_face.glb')
+  const {setAnimations} = useCharacterAnimations();
+  const { actions, names } = useAnimations(animations, group)
+
+  //The useEffect Hook allows you to perform side effects in your components.
+  useEffect(() => {
+    setAnimations(names)
+  }, [names])
+
+  useEffect(() => {
+    actions[names[0]].reset().fadeIn(0.5).play();
+  }, [])
   return (
-    <group {...props} dispose={null}>
-      <mesh castShadow receiveShadow geometry={nodes.EyeR.geometry} material={materials.Eye} />
-      <mesh castShadow receiveShadow geometry={nodes.EyeL.geometry} material={materials.Eye} />
-      <mesh castShadow receiveShadow geometry={nodes.ShoeL.geometry} material={materials.Shoes} />
-      <mesh castShadow receiveShadow geometry={nodes.ShoeR.geometry} material={materials.Shoes} />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.NurbsPath001.geometry}
-        material={materials.Hair1}
-      />
-      <mesh castShadow receiveShadow geometry={nodes.Body002.geometry} material={materials.Skin} />
-      <mesh castShadow receiveShadow geometry={nodes.Shirt.geometry} material={materials.Shirt} />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Trousers.geometry}
-        material={materials.Trousers}
-      />
+    <group ref={group} {...props} dispose={null}>
+      <group name="Scene">
+        <group
+          name="Armature003"
+          position={[-0.002, -0.002, 0.046]}
+          rotation={[-0.048, 0.008, -0.001]}>
+          <group name="Body003">
+            <skinnedMesh
+              name="Body008"
+              geometry={nodes.Body008.geometry}
+              material={materials['Skin.003']}
+              skeleton={nodes.Body008.skeleton}
+            />
+            <skinnedMesh
+              name="Body008_1"
+              geometry={nodes.Body008_1.geometry}
+              material={materials['Shirt.003']}
+              skeleton={nodes.Body008_1.skeleton}
+            />
+            <skinnedMesh
+              name="Body008_2"
+              geometry={nodes.Body008_2.geometry}
+              material={materials['Trousers.002']}
+              skeleton={nodes.Body008_2.skeleton}
+            />
+            <skinnedMesh
+              name="Body008_3"
+              geometry={nodes.Body008_3.geometry}
+              material={materials['Shoes.003']}
+              skeleton={nodes.Body008_3.skeleton}
+            />
+            <skinnedMesh
+              name="Body008_4"
+              geometry={nodes.Body008_4.geometry}
+              material={materials['Hair1.002']}
+              skeleton={nodes.Body008_4.skeleton}
+            />
+            <skinnedMesh
+              name="Body008_5"
+              geometry={nodes.Body008_5.geometry}
+              material={materials.Eye}
+              skeleton={nodes.Body008_5.skeleton}
+            />
+          </group>
+          <primitive object={nodes.Spine} />
+          <primitive object={nodes.LeftUpLeg} />
+          <primitive object={nodes.RightUpLeg} />
+          <primitive object={nodes.Hips} />
+        </group>
+      </group>
     </group>
   )
 }
 
-useGLTF.preload('./models/Irina_face14.glb')
+useGLTF.preload('./models/Irina_face.glb')
